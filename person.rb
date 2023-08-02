b6acaf00290d272a5e5dc3ab5c5482597ebbfcd9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-08-02 14:29:57 +0100

    Modify gem and person.rb

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 require 'json'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :parent_permission, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
